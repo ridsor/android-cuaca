b453d90ee9e395b96f57fd4e547cf4dbce804a4f
ridsor <97663231+ridsor@users.noreply.github.com>
2023-12-08 17:19:51 +0900

    Feature/home (#2)
     membuat ui home dan search

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,12 +1,73 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {FlatList, StyleSheet, TextInput, View} from 'react-native';
+import React, {useCallback} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import SearchItem from '../../components/Home/SearchItem';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
-export default function Search() {
+interface Props {
+  navigation: NativeStackNavigationProp<{Home: undefined}>;
+}
+
+export default function Search(props: Props) {
+  const handleSetLocation = useCallback(
+    (id: string) => {
+      props.navigation.navigate('Home');
+    },
+    [props.navigation],
+  );
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <LinearGradient colors={['#646EE9', '#704BF1']} style={styles.body}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Search..."
+          placeholderTextColor={'#c4c4c4'}
+          autoFocus
+          style={[styles.defaultFont, styles.searchInput]}
+        />
+        <View style={styles.searchWrapper}>
+          <FlatList
+            data={[
+              {id: '1', location: 'kemarin'},
+              {id: '1', location: 'kemarin'},
+              {id: '1', location: 'kemarin'},
+              {id: '1', location: 'kemarin'},
+            ]}
+            keyExtractor={item => item.id}
+            renderItem={data => (
+              <SearchItem data={data} onSetLocation={handleSetLocation} />
+            )}
+          />
+        </View>
+      </View>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    paddingTop: 65,
+  },
+  container: {
+    paddingHorizontal: 20,
+    flex: 1,
+  },
+  defaultFont: {
+    color: '#fff',
+    fontSize: 14,
+    fontFamily: 'Inter',
+  },
+  searchInput: {
+    height: 43,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    color: '#4a4a4a',
+    paddingHorizontal: 14,
+    marginBottom: 6,
+  },
+  searchWrapper: {
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+});
